f147e093f25606c6dc9269fa141aa204783e1740
gustavo domingues <deathdiec@gmail.com>
2022-09-06 08:04:56 -0300

    build bats now correctly place everything in the folders

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
-gcc main.c test.c input.c init.c draw.c hero.c SDL2.dll SDL2_image.dll --machine-windows -o heroes
+gcc main.c test.c input.c init.c draw.c hero.c SDL2.dll SDL2_image.dll --machine-windows -o dist/heroes
+xcopy /s resources dist\resources
--- debugBuild.bat
+++ debugBuild.bat
@@ -1,1 +1,2 @@
-gcc main.c test.c input.c init.c draw.c hero.c SDL2.dll SDL2_image.dll -o heroesDebug
+gcc main.c test.c input.c init.c draw.c hero.c SDL2.dll SDL2_image.dll -o debug/heroesDebug
+xcopy /s resources debug\resources
